d5b3c7c82e46ece47b55b9ad5e11e576e8b67d83
ElizavetaPanasiuk <lizaveta.panasiuk@gmail.com>
2021-01-27 19:04:10 +0300

    solving tasks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return array === undefined  || array.length === 0 ? 0 : array.reduce((maxElem, currentElem) => currentElem < maxElem ? currentElem : maxElem, array[0]);
 }
 
 exports.max = function max (array) {
-  return 0;
+  return array === undefined  || array.length === 0 ? 0 : array.reduce((maxElem, currentElem) => currentElem > maxElem ? currentElem : maxElem, array[0]);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return array === undefined  || array.length === 0 ? 0 : array.reduce((sum, currentElem) => sum + currentElem, 0) / array.length;
 }
